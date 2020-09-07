e8b299f608f5364767075f3e0f7acdbfa08feb33
MariannaCor <61152828+MariannaCor@users.noreply.github.com>
2020-09-07 23:48:38 +0200

    inviaAcconto implemented (Deploy test FAILED)

---
+++ src/main/java/org/acme/acme_project/InviaAcconto.java
@@ -0,0 +1,72 @@
+package org.acme.acme_project;
+
+import java.util.ArrayList;
+
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+
+public class InviaAcconto implements JavaDelegate {
+
+	private int insertedIdValue = -1;
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+
+		sendPost();
+
+		execution.getProcessEngineServices().getRuntimeService().createMessageCorrelation("RiceviConferma")
+				.setVariable("codiceBonifico", insertedIdValue).correlate();
+
+	}
+
+	private void sendPost() throws Exception {
+
+		HttpPost post = new HttpPost("http://localhost:3000/transfer");
+
+		// add request parameter, form parameters
+		List<NameValuePair> urlParameters = new ArrayList<>();
+		urlParameters.add(new BasicNameValuePair("receiver", "'AcmeBikes'"));
+		urlParameters.add(new BasicNameValuePair("sender", "'rivendita'"));
+		urlParameters.add(new BasicNameValuePair("transferAmount", "354"));
+
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+		CloseableHttpClient httpClient = null;
+		String responseString = null;
+		int lastInsertIdValue, beginServerStatus = 0;
+
+		try {
+			httpClient = HttpClients.createDefault();
+			CloseableHttpResponse response = httpClient.execute(post);
+
+			responseString = EntityUtils.toString(response.getEntity());
+
+		} finally {
+			httpClient.close();
+		}
+
+		if (responseString != null) {
+			// lavoro sulla stringa per ottenere il valore di "insertId"
+			lastInsertIdValue = responseString.indexOf("insertId") + 10;
+			beginServerStatus = responseString.indexOf("serverStatus") - 2;
+			insertedIdValue = Integer.parseInt(responseString.substring(lastInsertIdValue, beginServerStatus));
+			System.out.print(insertedIdValue);
+		}
+
+	}
+
+}
